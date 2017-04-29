b29912696839515cd21f31af7d79fe8ae1e03092
meglybbert <meglybbert@eagles.ewu.edu>
2017-04-29 13:53:45 -0700

    Added digital clock

--- js/scripts.js
+++ js/scripts.js
@@ -7,22 +7,52 @@ console.log('Hello, World!');
   const minsHand = document.querySelector('.min-hand');
   const hourHand = document.querySelector('.hour-hand');
 
-  function setDate() {
-    const now = new Date();
+  function setAnalogDate() {
+        const now = new Date();
 
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+        const seconds = now.getSeconds();
+        const secondsDegrees = ((seconds / 60) * 360) + 90;
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+        const mins = now.getMinutes();
+        const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+        minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-    const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+        const hour = now.getHours();
+        const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+        hourHand.style.transform = `rotate(${hourDegrees}deg)`;
   }
 
-  setInterval(setDate, 1000);
+    function setDigitalDate() {
+        var now = new Date()
+        var hour = now.getHours()
+        var min = now.getMinutes()
+        var sec = now.getSeconds()
+        var suffix = "AM";
 
-  setDate();
+        //to add AM or PM after time
+
+        if(hour>11) suffix = "PM";
+        if(hour>12) hour = hour-12;
+        if(hour==0) hour = 12;
+
+        //to add a placeholder 0 for one digit times
+
+        min=checkTime(min)
+        sec=checkTime(sec)
+
+        $('.digital').text(hour+":"+min+":"+sec+" "+suffix);
+        t=setTimeout('startTime()', 500)
+     }
+
+    function checkTime(i) {
+        if (i<10)
+        { i="0" + i}
+        return i
+    }
+
+    setInterval(setAnalogDate, 1000);
+    setInterval(setDigitalDate, 1000);
+
+    setAnalogDate();
+    setDigitalDate();
