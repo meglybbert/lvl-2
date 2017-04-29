399962ed4f4e20f5cc9b3ee2be69610c8a899a81
meglybbert <meglybbert@eagles.ewu.edu>
2017-04-29 13:10:52 -0700

    Starting buildoff

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,26 @@
 
 console.log('Hello, World!');
 
-function playSound(e){ 
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return; //stop the function
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
+  const secondHand = document.querySelector('.second-hand');
+  const minsHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
 
-    
-    
-}
-function removeTransition(e) {
-    if(e.propertyName !== 'transform')return;
-    
-    this.classList.remove('playing');
-}
+  function setDate() {
+    const now = new Date();
 
-const keys = document.querySelectorAll('.key');
-console.log(keys);
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-window.addEventListener('keydown', playSound);
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  }
+
+  setInterval(setDate, 1000);
+
+  setDate();
